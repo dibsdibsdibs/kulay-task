ef299d9be3b7c5f1366ddc73ba6ed3a1c639cefc
Divine <divinelavente@gmail.com>
2026-02-26 16:03:55 +0800

    redux state setup

--- kulaysample/app/_layout.tsx
+++ kulaysample/app/_layout.tsx
@@ -2,29 +2,33 @@ import { Button } from "@react-navigation/elements";
 import "../global.css";
 import { Stack } from "expo-router";
 import CartButton from "../components/header/CartButton";
+import { Provider } from "react-redux";
+import { store } from "../utils/store";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "KulaySampleMenu",
-          headerRight: () => <CartButton />,
-        }}
-      />
-      <Stack.Screen
-        name="products/[id]"
-        options={{
-          title: "Details",
-        }}
-      />
-      <Stack.Screen
-        name="cart/cart"
-        options={{
-          title: "Order Details",
-        }}
-      />
-    </Stack>
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "KulaySampleMenu",
+            headerRight: () => <CartButton />,
+          }}
+        />
+        <Stack.Screen
+          name="products/[id]"
+          options={{
+            title: "Details",
+          }}
+        />
+        <Stack.Screen
+          name="cart/cart"
+          options={{
+            title: "Order Details",
+          }}
+        />
+      </Stack>
+    </Provider>
   );
 }
---
+++ kulaysample/utils/store.ts
@@ -0,0 +1,42 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartItem } from "@/types/cart";
+
+type CartState = {
+    items: CartItem[];
+};
+
+const initialState: CartState = {
+    items: [],
+};
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action: PayloadAction<CartItem>) => {
+        const existing = state.items.find(i => i.id === action.payload.id);
+            if (existing) {
+                existing.quantity += action.payload.quantity;
+            } else {
+                state.items.push(action.payload);
+            }
+        },
+        removeFromCart: (state, action: PayloadAction<string>) => {
+            state.items = state.items.filter(i => i.id !== action.payload);
+        },
+        clearCart: (state) => {
+            state.items = [];
+        },
+    },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+
+export const store = configureStore({
+    reducer: {
+        cart: cartSlice.reducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
